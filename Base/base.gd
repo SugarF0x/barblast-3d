0f0ef5b5fadf380d71ce685c328d793375b47908
Alexey Tuzov <sugarfox.dev@gmail.com>
2024-02-07 02:55:58 +0300

    add health habel

--- Base/base.gd
+++ Base/base.gd
@@ -1,6 +1,9 @@
 extends Node3D
 
 
+@onready var health_label: Label3D = $HealthLabel
+
+
 @export var max_health: int = 10
 
 
@@ -9,8 +12,12 @@ var health: int
 
 func _ready():
 	health = max_health
+	sync_health_label()
 
+
+func sync_health_label():
+	health_label.text = str(health)
 
 func take_damage(value: int) -> void:
 	health -= value
-	print(health)
+	sync_health_label()
