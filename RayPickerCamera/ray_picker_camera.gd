732ade892068bf591d81677fbf26f862f2098540
Alexey Tuzov <sugarfox.dev@gmail.com>
2024-02-07 23:48:45 +0300

    add mouse click events
    update tile item on click

--- RayPickerCamera/ray_picker_camera.gd
+++ RayPickerCamera/ray_picker_camera.gd
@@ -1,10 +1,20 @@
 extends Camera3D
 
 
+@export var grid_map: GridMap
+
+
 @onready var ray_cast_3d: RayCast3D = $RayCast3D
 
 
 func _process(delta: float) -> void:
 	ray_cast_3d.target_position = project_local_ray_normal(get_viewport().get_mouse_position()) * 100.0
 	ray_cast_3d.force_raycast_update()
-	print(ray_cast_3d.get_collider())
+	
+	if not ray_cast_3d.is_colliding(): return
+	var collider = ray_cast_3d.get_collider()
+	
+	if collider is GridMap:
+		var collision_point = ray_cast_3d.get_collision_point()
+		var cell = grid_map.local_to_map(collision_point)
+		if Input.is_action_just_pressed("click") and grid_map.get_cell_item(cell) == 0: grid_map.set_cell_item(cell, 1)
