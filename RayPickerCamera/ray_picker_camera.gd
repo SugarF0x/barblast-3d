00bd1a894cd7081fcb497dec61b8c27f59e3bf7c
Alexey Tuzov <sugarfox.dev@gmail.com>
2024-02-07 23:57:59 +0300

    add cursor states based on action availability

--- RayPickerCamera/ray_picker_camera.gd
+++ RayPickerCamera/ray_picker_camera.gd
@@ -11,10 +11,18 @@ func _process(delta: float) -> void:
 	ray_cast_3d.target_position = project_local_ray_normal(get_viewport().get_mouse_position()) * 100.0
 	ray_cast_3d.force_raycast_update()
 	
-	if not ray_cast_3d.is_colliding(): return
+	if not ray_cast_3d.is_colliding(): return reset_pointer()
 	var collider = ray_cast_3d.get_collider()
 	
-	if collider is GridMap:
-		var collision_point = ray_cast_3d.get_collision_point()
-		var cell = grid_map.local_to_map(collision_point)
-		if Input.is_action_just_pressed("click") and grid_map.get_cell_item(cell) == 0: grid_map.set_cell_item(cell, 1)
+	if not collider is GridMap: return reset_pointer()
+	
+	var collision_point = ray_cast_3d.get_collision_point()
+	var cell = grid_map.local_to_map(collision_point)
+	
+	if grid_map.get_cell_item(cell) == 0:
+		Input.set_default_cursor_shape(Input.CURSOR_POINTING_HAND)
+		if Input.is_action_just_pressed("click"): grid_map.set_cell_item(cell, 1)
+	else: reset_pointer()
+
+
+func reset_pointer() -> void: Input.set_default_cursor_shape(Input.CURSOR_ARROW)
