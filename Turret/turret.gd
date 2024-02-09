a5bb5ef8ff0b80de11c2c8066db4e36485766542
Alexey Tuzov <sugarfox.dev@gmail.com>
2024-02-09 20:19:04 +0300

    have turrets follow first enemy

--- Turret/turret.gd
+++ Turret/turret.gd
@@ -7,6 +7,14 @@ extends Node3D
 @onready var barrel: MeshInstance3D = %Barrel
 
 
+var enemy_path: Path3D
+
+
+func _physics_process(delta: float) -> void:
+	var enemy = enemy_path.get_children().back()
+	look_at(enemy.global_position, Vector3.UP, true)
+
+
 func fire():
 	var projectile = projectile_scene.instantiate()
 	add_child(projectile)
--- TurretManager.gd
+++ TurretManager.gd
@@ -4,7 +4,11 @@ extends Node3D
 @export var turret_scene: PackedScene
 
 
+@onready var enemy_path: Path3D = %EnemyPath
+
+
 func build_turret(pos: Vector3) -> void:
 	var turret = turret_scene.instantiate()
 	add_child(turret)
 	turret.global_position = pos
+	turret.enemy_path = enemy_path
