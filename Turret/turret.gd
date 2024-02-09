4a3d62c8ead0a8d37781af1611abd2c91664306c
Alexey Tuzov <sugarfox.dev@gmail.com>
2024-02-09 20:26:38 +0300

    set projectile direction towards enemy target

--- Turret/turret.gd
+++ Turret/turret.gd
@@ -8,17 +8,20 @@ extends Node3D
 
 
 var enemy_path: Path3D
+var target_position: Vector3
 
 
 func _physics_process(delta: float) -> void:
-	var enemy = enemy_path.get_children().back()
-	look_at(enemy.global_position, Vector3.UP, true)
+	target_position = enemy_path.get_children().back().global_position
+	look_at(target_position, Vector3.UP, true)
 
 
 func fire():
 	var projectile = projectile_scene.instantiate()
 	add_child(projectile)
 	projectile.global_position = barrel.global_position
+	projectile.direction = projectile.global_position.direction_to(target_position)
+	projectile.direction.y = 0
 
 
 func _on_fire_rate_timer_timeout() -> void: fire()
