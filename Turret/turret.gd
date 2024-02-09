8b542d79daf3c982f8d79d3ed55ecfbb6d8315b0
Alexey Tuzov <sugarfox.dev@gmail.com>
2024-02-09 23:49:40 +0300

    stop shooting on no targets left

--- Turret/turret.gd
+++ Turret/turret.gd
@@ -8,25 +8,28 @@ extends Node3D
 
 
 var enemy_path: Path3D
+var target: PathFollow3D
 
 
 func _physics_process(delta: float) -> void:
-	var target = find_best_target()
+	find_best_target()
 	if not target: return
 	
 	look_at(target.global_position, Vector3.UP, true)
 
 
 func fire():
+	if not target: return
+	
 	var projectile = projectile_scene.instantiate()
 	add_child(projectile)
 	projectile.global_position = barrel.global_position
 	projectile.direction = global_transform.basis.z
 
 
-func find_best_target() -> PathFollow3D:
+func find_best_target() -> void:
 	var enemies = enemy_path.get_children().filter(func(child): return child is PathFollow3D)
-	return enemies.reduce(func(acc: PathFollow3D, val: PathFollow3D): return val if acc.progress_ratio < val.progress_ratio else acc)
+	target = enemies.reduce(func(acc: PathFollow3D, val: PathFollow3D): return val if acc.progress_ratio < val.progress_ratio else acc)
 
 
 func _on_fire_rate_timer_timeout() -> void: fire()
