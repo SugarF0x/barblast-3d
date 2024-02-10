9dc9ec157eb1ac85930abe6251d6b836d46aa931
Alexey Tuzov <sugarfox.dev@gmail.com>
2024-02-10 19:41:18 +0300

    Add victory screen breakdown

--- UI/victory_layer.gd
+++ UI/victory_layer.gd
@@ -5,15 +5,26 @@ extends CanvasLayer
 @onready var retry_button: Button = %RetryButton
 @onready var quit_button: Button = %QuitButton
 @onready var stars_container: HBoxContainer = %StarsContainer
+@onready var breakdown_labels: VBoxContainer = %BreakdownLabels
 @onready var base = get_tree().get_first_node_in_group("base")
 
 func victory(survived: bool, at_max_health: bool, plenty_of_gold: bool) -> void:
 	visible = true
 	
 	var stars = stars_container.get_children()
-	stars[0].modulate = Color.WHITE if survived else Color.BLACK
-	stars[1].modulate = Color.WHITE if at_max_health else Color.BLACK
-	stars[2].modulate = Color.WHITE if plenty_of_gold else Color.BLACK
+	set_condition(survived, stars[0], "Survived +1")
+	set_condition(at_max_health, stars[1], "Full health +1")
+	set_condition(plenty_of_gold, stars[2], "Over 300 gold +1")
+
+func set_condition(value: bool, star: TextureRect, text: String) -> void:
+	if not value: 
+		star.modulate = Color.BLACK
+		return
+	
+	star.modulate = Color.WHITE
+	var breakdown_label = Label.new()
+	breakdown_labels.add_child(breakdown_label)
+	breakdown_label.text = text
 
 func retry() -> void: get_tree().reload_current_scene()
 
