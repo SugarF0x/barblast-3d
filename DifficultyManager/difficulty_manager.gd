43398c0a707007fd1c2b119a148e59f3fa68018c
Alexey Tuzov <sugarfox.dev@gmail.com>
2024-02-10 14:10:10 +0300

    add decreasing enemy spawn timer

---
+++ DifficultyManager/difficulty_manager.gd
@@ -0,0 +1,18 @@
+class_name DifficultyManager
+extends Node
+
+
+@export var game_length := 30
+@export var spawn_time_curve: Curve
+
+
+@onready var level_duration_timer: Timer = %LevelDurationTimer
+
+
+func _ready() -> void:
+	level_duration_timer.start(game_length)
+
+
+func game_progress_ratio() -> float: return 1.0 - level_duration_timer.time_left / game_length
+
+func get_spawn_time() -> float: return spawn_time_curve.sample(game_progress_ratio())
--- EnemyPath.gd
+++ EnemyPath.gd
@@ -2,9 +2,21 @@ extends Path3D
 
 
 @export var enemy_scene: PackedScene
+@export var difficulty_manager: DifficultyManager
+
+
+@onready var enemy_spawn_timer: Timer = %EnemySpawnTimer
+
+
+func _ready() -> void:
+	set_timer()
 
 
 func spawn_enemy() -> void: add_child(enemy_scene.instantiate())
 
+func set_timer() -> void: enemy_spawn_timer.start(difficulty_manager.get_spawn_time())
 
-func _on_enemy_spawn_timer_timeout() -> void: spawn_enemy()
+
+func _on_enemy_spawn_timer_timeout() -> void: 
+	spawn_enemy()
+	set_timer()
