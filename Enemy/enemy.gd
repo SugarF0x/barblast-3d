33d988790c00c367eefa237bd42a9723e7b0d840
Alexey Tuzov <sugarfox.dev@gmail.com>
2024-02-10 13:56:01 +0300

    add enemy kill bounty

--- Enemy/enemy.gd
+++ Enemy/enemy.gd
@@ -2,9 +2,11 @@ extends PathFollow3D
 
 
 @export_range(1.0,10.0) var speed := 5.0
+@export var bounty := 15
 
 
 @onready var base = get_tree().get_first_node_in_group("base")
+@onready var bank = get_tree().get_first_node_in_group("bank")
 @onready var health_component: HealthComponent = $HealthComponent
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 
@@ -23,4 +25,9 @@ func take_damage(value: int):
 	animation_player.play("DamageFlash")
 	health_component.damage(value)
 
-func _on_health_component_death() -> void: queue_free()
+func die():
+	bank.gold += bounty
+	queue_free()
+
+
+func _on_health_component_death() -> void: die()
