8a8dcecf507d4607eda868c118991c637ae94db9
Alexey Tuzov <sugarfox.dev@gmail.com>
2024-02-11 13:40:04 +0300

    add damage var to projectile

--- Turret/projectile.gd
+++ Turret/projectile.gd
@@ -2,6 +2,7 @@ extends Area3D
 
 
 @export var speed := 30.0
+@export var damage := 1
 
 
 var direction := Vector3.FORWARD
@@ -14,5 +15,5 @@ func _physics_process(delta: float) -> void:
 func _on_ttl_timer_timeout() -> void: queue_free()
 
 func _on_area_entered(area: Area3D) -> void:
-	if area.is_in_group("enemy_area"): area.get_parent().take_damage(1)
+	if area.is_in_group("enemy_area"): area.get_parent().take_damage(damage)
 	queue_free()
